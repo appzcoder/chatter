ac2fb27999db69b68635cc937b693bf4831fc065
Sohel Amin <sohelamincse@gmail.com>
2017-05-02 00:16:54 +0600

    Chatting app added

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
-var app = require('express')();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var express = require('express');
+var path = require('path');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var _ = require('lodash');
+var port = process.env.PORT || 3000;
 
-server.listen(3000);
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function (req, res) {
-    res.sendfile(__dirname + '/index.html');
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function (socket) {
-    socket.on('user-added', function (user) {
-        console.log(user);
-        socket.broadcast.emit('user-added', user);
+var users = [];
+
+io.on('connection', function(socket) {
+    socket.on('online user', function(user) {
+        user.socket_id = socket.id;
+
+        var foundUser = _.find(users, {id: user.id});
+
+        if (foundUser) {
+            foundUser.socket_id = socket.id;
+        } else {
+            users.push(user);
+        }
+
+        io.emit('online users list', users);
     });
+
+    socket.on('chat message', function(msg, toUserID, from) {
+        var toUser = _.find(users, {id: toUserID});
+
+        socket.to(toUser.socket_id).emit('chat message', msg, from);
+    });
+
+    socket.on('disconnect', function() {
+        users = _.reject(users, {socket_id: socket.id});
+    });
+});
+
+
+http.listen(port, function(){
+    console.log('listening on *:' + port);
 });
